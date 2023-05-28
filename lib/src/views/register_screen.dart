0a82b5a77095f5c45fa0d16c3e9faace6be7af85
Danial Othman <danialbinothman@gmail.com>
2023-05-28 16:29:15 +0800

    Added registration cards for different roles

--- lib/src/views/register_screen.dart
+++ lib/src/views/register_screen.dart
@@ -20,7 +20,86 @@ class _RegisterScreenState extends State<RegisterScreen> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
-              children: [Container()],
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    debugPrint("Go to PARENT/GUARDIAN FORM");
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 128,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                                right: 0,
+                                bottom: 0,
+                                child: Text(
+                                  "Parent / Guardian",
+                                  style:
+                                      Theme.of(context).textTheme.headlineSmall,
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    debugPrint("Go to SCHOOL ADMIN FORM");
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 128,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                                right: 0,
+                                bottom: 0,
+                                child: Text(
+                                  "School Admin",
+                                  style:
+                                      Theme.of(context).textTheme.headlineSmall,
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    debugPrint("Go to BUS DRIVER FORM");
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 128,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                                right: 0,
+                                bottom: 0,
+                                child: Text(
+                                  "Bus Driver",
+                                  style:
+                                      Theme.of(context).textTheme.headlineSmall,
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),

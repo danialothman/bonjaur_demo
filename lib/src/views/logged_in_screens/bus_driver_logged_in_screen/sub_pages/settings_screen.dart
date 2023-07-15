1e7b7a3e1dd081b895c7bb1580f840af3d1556ed
Danial Othman <danialbinothman@gmail.com>
2023-07-15 23:00:35 +0800

    Update settings_screen.dart

--- lib/src/views/logged_in_screens/bus_driver_logged_in_screen/sub_pages/settings_screen.dart
+++ lib/src/views/logged_in_screens/bus_driver_logged_in_screen/sub_pages/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -10,6 +11,25 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Settings");
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          title: const Text('Common'),
+          tiles: <SettingsTile>[
+            SettingsTile.navigation(
+              leading: const Icon(Icons.language),
+              title: const Text('Language'),
+              value: const Text('English'),
+            ),
+            SettingsTile.switchTile(
+              onToggle: (value) {},
+              initialValue: true,
+              leading: const Icon(Icons.format_paint),
+              title: const Text('Enable custom theme'),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
